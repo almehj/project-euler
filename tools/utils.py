42a163e67cde157d00a84ab680ea7c29688fcf51
Hank Alme <Hank.Alme@jhuapl.edu>
2019-04-11 11:41:58 -0400

    Solution to #36

--- tools/utils.py
+++ tools/utils.py
@@ -4,3 +4,11 @@ def seq_string(l,**kwargs):
     sep = kwargs.get("separator"," ")
 
     return sep.join([str(n) for n in l])
+
+def binary_str(n):
+    answer = []
+    while n > 0:
+        answer.append(n%2)
+        n //= 2
+    return seq_string(reversed(answer),separator="")
+
