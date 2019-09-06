1b8a3256fedb71e69e247f1b32754ca739ba7653
Hank Alme <hank.alme@jhuapl.edu>
2019-09-06 16:29:41 -0400

    More work on #100

--- problem00100/bisect_experiement.py
+++ problem00100/bisect_experiement.py
@@ -11,6 +11,16 @@ def calc_blue(n):
     return .5*(1. + sqrt(n_term(n)))
     
 
+def find_min_total(n):
+
+    disc = 1 - 2*(1 - n**2)
+    if disc < 0:
+        return 0
+    flt_val = .5*(1 + sqrt(disc))
+
+    return int(flt_val)
+
+
 def find_perfect(t_min):
     n = 1
     nsq = 1
@@ -26,14 +36,25 @@ def find_perfect(t_min):
             return 
         t += 1
 
-
-
-
 def main():
 
+
+    n = int(sys.argv[1])
+
+    print(n_term(n))
+    sys.exit(10)
     
-    find_perfect(int(sys.argv[1]))
-    
+    for i in range(n,n*10):
+        #print("%d %d: %d"%(i,i**2,find_min_total(i)))
+        n = find_min_total(i)
+        while n_term(n) <= i**2:
+            if n_term(n) == i**2:
+                print("   *** FOUND: %d => %d == %d"%(n,n_term(n),i**2))
+                print("              %d blue, %d red"%(calc_blue(n),n-calc_blue(n)))
+                sys.exit()
+
+            n += 1
+        
 #    for val in sys.argv[1:]:
 #        n = int(val)
 #        prev_nt = n_term(n)
